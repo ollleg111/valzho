d206f651a6fba167be619dd9871911e2aefab794
Solod <jack_killer@mail.ru>
2016-08-28 09:56:21 +0300

    close drawer after selected

--- app/src/main/java/com/solodilov/evgen/valzho/activitys/BaseActivity.java
+++ app/src/main/java/com/solodilov/evgen/valzho/activitys/BaseActivity.java
@@ -3,6 +3,7 @@ package com.solodilov.evgen.valzho.activitys;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import com.solodilov.evgen.valzho.Seasons;
 
 public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static FirebaseDatabase firebaseDatabase;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +51,33 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigati
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+        switch (item.getItemId()) {
+            case R.id.app_resume:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_winter:
+                displaySelectedSeason(Seasons.WINTER);
+                break;
+            case R.id.menu_spring:
+                displaySelectedSeason(Seasons.SPRING);
+                break;
+            case R.id.menu_summer:
+                displaySelectedSeason(Seasons.SUMMER);
+                break;
+            case R.id.menu_autumn:
+                displaySelectedSeason(Seasons.AUTUMN);
+                break;
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     protected void initDrawerNav(Toolbar mToolbar) {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -64,4 +85,13 @@ public abstract class BaseActivity extends AppCompatActivity implements Navigati
     }
 
     protected abstract void displaySelectedSeason(Seasons seasons);
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
