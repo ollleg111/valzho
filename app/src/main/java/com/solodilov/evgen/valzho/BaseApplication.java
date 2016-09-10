fd4f429634b1008133140ef30c7d57a31d22072f
Solod <jack_killer@mail.ru>
2016-09-10 12:03:58 +0300

    edit DraverNavigation edit ProgressBar to FullCardModel

--- app/src/main/java/com/solodilov/evgen/valzho/BaseApplication.java
+++ app/src/main/java/com/solodilov/evgen/valzho/BaseApplication.java
@@ -2,19 +2,22 @@ package com.solodilov.evgen.valzho;
 
 import android.app.Application;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
 public class BaseApplication extends Application {
+    private static FirebaseDatabase firebaseDatabase;
     @Override
     public void onCreate() {
         super.onCreate();
-
+        if (firebaseDatabase == null) {
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            firebaseDatabase.setPersistenceEnabled(true);
+        }
         Picasso.Builder builder = new Picasso.Builder(this);
         builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
         Picasso built = builder.build();
-        //built.setIndicatorsEnabled(true);
-        //built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
     }
 }
--- app/src/main/java/com/solodilov/evgen/valzho/activitys/BaseActivity.java
+++ app/src/main/java/com/solodilov/evgen/valzho/activitys/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.solodilov.evgen.valzho.activitys;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.firebase.database.FirebaseDatabase;
 import com.solodilov.evgen.valzho.R;
 import com.solodilov.evgen.valzho.Seasons;
 
 public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private static FirebaseDatabase firebaseDatabase;
     private DrawerLayout mDrawerLayout;
 
     public DrawerLayout getDrawerLayout() {
         return mDrawerLayout;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (firebaseDatabase == null) {
-            firebaseDatabase = FirebaseDatabase.getInstance();
-            firebaseDatabase.setPersistenceEnabled(true);
-        }
     }
 
     @Override
